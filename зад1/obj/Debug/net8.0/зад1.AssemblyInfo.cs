7fa17b25563cc44a75ff5f9ff628b893e590210e
zhemhy <zhemchy@gmail.com>
2023-06-16 17:05:56 +0300

    <commit3>

--- зад1/obj/Debug/net8.0/зад1.AssemblyInfo.cs
+++ зад1/obj/Debug/net8.0/зад1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("зад1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ef6ce876e8b2be195da02af74b22a6f87a722e")]
 [assembly: System.Reflection.AssemblyProductAttribute("зад1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("зад1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
